4da9f8af94ae3662a7c3cb6300a5a56a86a0a27a
Aniruddha <aniruddhag87@live.com>
2022-07-09 12:21:26 +0530

    initial commit

--- Exercise/Assignment1.1/13.py
+++ Exercise/Assignment1.1/13.py
@@ -17,28 +17,28 @@ print("New List: ", l)
 
 # ______________________________________________________________________________________________________________________
 
-def remove_wrd(lst, wrd, n):
-    count = 0
-
-    for i in range(0, len(lst)):
-        if lst[i] == wrd:
-            count = count + 1
-
-            if count == n:
-                del(lst[i])
-                return True
-
-    return False
-
-
-lst = ["Ani", "Pankaj", "Ravi", "Shankar", "Mahesh", "Ravi", "Mahesh", "Ani", "Ravi"]
-print("The list is: ")
-print(lst)
-wrd = "Ravi"
-n = 2
-ele = remove_wrd(lst, wrd, n)
-
-if ele == True:
-    print("The updated list: ", lst)
-else:
-    print("New list: ")
+# def remove_wrd(lst, wrd, n):
+#     count = 0
+#
+#     for i in range(0, len(lst)):
+#         if lst[i] == wrd:
+#             count = count + 1
+#
+#             if count == n:
+#                 del(lst[i])
+#                 return True
+#
+#     return False
+#
+#
+# lst = ["Ani", "Pankaj", "Ravi", "Shankar", "Mahesh", "Ravi", "Mahesh", "Ani", "Ravi"]
+# print("The list is: ")
+# print(lst)
+# wrd = "Ravi"
+# n = 2
+# ele = remove_wrd(lst, wrd, n)
+#
+# if ele == True:
+#     print("The updated list: ", lst)
+# else:
+#     print("New list: ")
