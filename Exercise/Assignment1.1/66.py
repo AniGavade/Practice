fd4c49254b53ca27b48cb28efbbde38a22a0a84e
Aniruddha <aniruddhag87@live.com>
2022-07-10 11:57:08 +0530

    Anagram word

--- Exercise/Assignment1.1/66.py
+++ Exercise/Assignment1.1/66.py
@@ -1,21 +1,19 @@
 # Python Counter to find the size of largest subset of anagram words
 #
-from collections import Counter
+# from collections import Counter
 
 
-def largeSubset(word):
-
-    word = word.split(" ")
-
-    for i in range(0, len(word)):
-        word[i] = "".join(sorted(word[i]))
-
-    a = Counter(word)
-    print(max(a.values()))
-
-
-if __name__ == "__main__":
-    word = "rukna evol ylfrettub tub vole ylfrettub evol knura"
-    largeSubset(word)
-
-
+# def largeSubset(word):
+#
+#     word = word.split(" ")
+#
+#     for i in range(0, len(word)):
+#         word[i] = "".join(sorted(word[i]))
+#
+#     a = Counter(word)
+#     print(max(a.values()))
+#
+#
+# if __name__ == "__main__":
+#     word = "rukna evol ylfrettub tub vole ylfrettub evol knura"
+#     largeSubset(word)
