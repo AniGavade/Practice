8c4760390d7f7fa8e872220feaf04919150b4fa6
Aniruddha <aniruddhag87@live.com>
2022-07-11 14:53:24 +0530

    String Contain all Vowels

---
+++ Exercise/Assignment1.1/41.py
@@ -0,0 +1,34 @@
+# Ways to remove i’th character from string
+
+# l1 = input("Enter the string: ")
+# a1 = int(input("enter the number element to be removed: "))
+# b1 = ""
+# for i in range(len(l1)):
+#     if i != a1:
+#         b1 += l1[i]
+# print(l1)
+# print(b1)
+# ______________________________________________________________________________________________________________________
+
+# str_ = input('Enter the string : ')
+# a = int(input('Enter the index of character to be removed : '))
+#
+# b = str_.replace(str_[a], "a", 1)
+#
+# print("Entered string : ", str_)
+# print("String formed by removing i'th character : ", b)
+# ______________________________________________________________________________________________________________________
+
+# str_ = input('Enter the string : ')
+# a = int(input('Enter the index of character to be removed : '))
+#
+# b = str_[:a] + str_[(a+1):]
+#
+# print("Entered string : ", str_)
+# print("String formed by removing i'th character : ", b)
+# ______________________________________________________________________________________________________________________
+
+x = input("enter the string : ")
+y = input("which character you want to remove: ")
+z = x.replace(y, "")
+print(z)
