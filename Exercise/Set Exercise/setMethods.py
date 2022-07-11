af432d39c358af0e0359d0c9cb840a69f31934ea
Aniruddha <aniruddhag87@live.com>
2022-07-11 15:58:10 +0530

    initial commit

---
+++ Exercise/Set Exercise/setMethods.py
@@ -0,0 +1,22 @@
+# Union = '''It combines two or more set in single set without any duplicate value.'''
+
+# x = {1, 2, 3, 4, 5}
+# y = {6, 7, 8, 9, 1, 2}
+# z = x.union(y)
+# print(z)
+
+# x = {"wonder", "self", "image", "git"}
+# y = {'python', "java", "HTML", "git", "C++`"}
+# z = x.union(y)
+# print(z)
+# ______________________________________________________________________________________________________________________
+
+# intersection() = '''It gives output common values between given variables'''
+
+# x = {"wonder", "self", "image", "git"}
+# y = {'python', "java", "HTML", "git", "C++"}
+# z = x.intersection(y)
+# print(z)
+# ______________________________________________________________________________________________________________________
+
+intersection_update() = '''It '''
