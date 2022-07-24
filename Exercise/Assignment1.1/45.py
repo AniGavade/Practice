ee6a30313a4ec0d0a4a9c5d73d2bdbe06009040f
Aniruddha <aniruddhag87@live.com>
2022-07-24 19:48:20 +0530

    initial commit

--- Exercise/Assignment1.1/45.py
+++ Exercise/Assignment1.1/45.py
@@ -1,14 +1,15 @@
 # Count the Number of matching characters in a pair of string
 
 def count(s1, s2):
-    c=0 #counter variable
-    j=0
+    c = 0 #counter variable
+    j = 0
     for i in s1:
-        if s2.find(i)>-0 and j==s1.find(i):
-            c=c+1
-        j=j+1
-    print("Matching char: ",c)
+        if s2.find(i) > -0 and j == s1.find(i):
+            c = c+1
+        j = j+1
+    print("Matching char: ", c)
 
-s1="aabcdefk12"
-s2="bafk145"
-count(s1,s2)
+
+s1 = "aabcdefk12"
+s2 = "bafk145"
+count(s1, s2)
