e0b98135d2c1ac7cb05dd8d7efc81333b3fee1e7
Aniruddha <aniruddhag87@live.com>
2022-08-02 13:44:11 +0530

    initial commit

---
+++ Frm Prathamesh/python_built_in_functions/abs().py
@@ -0,0 +1,16 @@
+# The abs() function returns the absolute value of the given number. If the number is a complex number,
+# abs() returns its magnitude.
+
+# For integers - integer absolute value is returned
+print(abs(-10))
+print(abs(10))
+
+# For floating numbers - floating absolute value is returned
+print(abs(-3.3))
+print(abs(3.3))
+
+# For complex numbers - magnitude of the number is returned
+print(abs(5 - 5j))
+print(abs(5 + 5j))
+
+# magnitude of complex number a + bi = root(sq(a) + sq(b))
